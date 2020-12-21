f29fedb48a798a2d43751a20009d401719d5f93b
RestarDEV <Restardev.2002@gmail.com>
2020-12-21 22:45:44 +0700

    Update

--- lib/screens/show_shop_tree_menu.dart
+++ lib/screens/show_shop_tree_menu.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:treeshop/model/user_model.dart';
+import 'package:treeshop/utility/my_style.dart';
+import 'package:treeshop/widget/about_shop.dart';
+import 'package:treeshop/widget/show_menu_tree.dart';
 
 class ShowShopTreeMenu extends StatefulWidget {
   final UserModel userModel;
@@ -10,12 +13,29 @@ class ShowShopTreeMenu extends StatefulWidget {
 
 class _ShowShopTreeMenuState extends State<ShowShopTreeMenu> {
   UserModel userModel;
+  List<Widget> listWigets = List();
+  int indexPage = 0;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     userModel = widget.userModel;
+    listWigets.add(AboutShop(userModel: userModel,));
+    listWigets.add(ShowMenuTree());
+  }
+
+  BottomNavigationBarItem aboutShopNav() {
+    return BottomNavigationBarItem(
+      icon: Icon(Icons.restaurant),
+      title: Text('รายละเอียดร้าน'),
+    );
+  }
+
+  BottomNavigationBarItem showMenuTreeNav() {
+    return BottomNavigationBarItem(
+      icon: Icon(Icons.restaurant_menu),
+      title: Text('เมนูต้นไม้'),
+    );
   }
 
   @override
@@ -24,6 +44,23 @@ class _ShowShopTreeMenuState extends State<ShowShopTreeMenu> {
       appBar: AppBar(
         title: Text(userModel.nameShop),
       ),
+      body: listWigets.length == 0 ? MyStyle().showProgress() : listWigets[indexPage] ,
+      //body: Text('Test'),
+      bottomNavigationBar: ShowBottonNavigationBar(),
     );
   }
+
+  BottomNavigationBar ShowBottonNavigationBar() => BottomNavigationBar(
+        selectedItemColor: Colors.green,
+        currentIndex: indexPage,
+        onTap: (value) {
+          setState(() {
+            indexPage = value;
+          });
+        },
+        items: <BottomNavigationBarItem>[
+          aboutShopNav(),
+          showMenuTreeNav(),
+        ],
+      );
 }
---
+++ lib/widget/show_menu_tree.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShowMenuTree extends StatefulWidget {
+  @override
+  _ShowMenuTreeState createState() => _ShowMenuTreeState();
+}
+
+class _ShowMenuTreeState extends State<ShowMenuTree> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'This is ShowMenuTree'
+    );
+  }
+}
